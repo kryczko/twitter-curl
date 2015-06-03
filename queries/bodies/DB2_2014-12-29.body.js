01a247e7dfa73bbe8d1c74d0bb2a0dcf9eee908e
Kevin Ryczko <kevinryczko@me.com>
2015-06-03 14:53:43 -0400

    minor fix with outputted json files

--- queries/bodies/DB2_2014-12-29.body.js
+++
@@ -1,1 +0,0 @@
-{"content":{"date": "2014-12-29","title": "DB2"},{"search":{"results":0,"current":0},"tweets":[],"related":{"next":{"href":"https://cdeservice.mybluemix.net/api/v1/messages/search?q=Database+AND+12C+AND+%28features+OR+happy+OR+love+OR+like+OR+enjoy+OR+great+OR+awesome+OR+helpful%29+posted%3A2014-12-29%2C2014-12-30&from=0&size=500"}}}}
